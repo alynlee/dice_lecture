2a59f0ac72ad6e3840fccb49b02491adfc32b529
alyn <howerll12@gmail.com>
2021-12-16 16:09:46 +0900

    implement logic

--- app/src/main/java/com/androidlearning/dice_lecture/MainActivity.kt
+++ app/src/main/java/com/androidlearning/dice_lecture/MainActivity.kt
@@ -2,10 +2,12 @@ package com.androidlearning.dice_lecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.androidlearning.dice_lecture.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,8 +26,31 @@ class MainActivity : AppCompatActivity() {
 //        }
 
         //2번방식 - DataBinding
-        binding.testBtnId.setOnClickListener{
+        val diceImage1 = binding.dice1
+        val diceImage2 = binding.dice2
+
+        binding.diceStartBtn.setOnClickListener{
             Toast.makeText(this, "click", Toast.LENGTH_LONG).show()
+
+            val first = Random.nextInt(1,6)
+            val second = Random.nextInt(1,6)
+//            Log.d("MainActivity", first.toString())
+//            Log.d("MainActivity", second.toString())
+
+            if (first == 1) diceImage1.setImageResource(R.drawable.dice_1)
+            else if (first == 2) diceImage1.setImageResource(R.drawable.dice_2)
+            else if (first == 3) diceImage1.setImageResource(R.drawable.dice_3)
+            else if (first == 4) diceImage1.setImageResource(R.drawable.dice_4)
+            else if (first == 5) diceImage1.setImageResource(R.drawable.dice_5)
+            else diceImage1.setImageResource(R.drawable.dice_6)
+
+            if (second == 1) diceImage2.setImageResource(R.drawable.dice_1)
+            else if (second == 2) diceImage2.setImageResource(R.drawable.dice_2)
+            else if (second == 3) diceImage2.setImageResource(R.drawable.dice_3)
+            else if (second == 4) diceImage2.setImageResource(R.drawable.dice_4)
+            else if (second == 5) diceImage2.setImageResource(R.drawable.dice_5)
+            else diceImage2.setImageResource(R.drawable.dice_6)
+
         }
     }
 }
