726e9defdbb7535fb55ebf87eb5b74b06dcc482a
alyn <howerll12@gmail.com>
2021-12-16 15:03:11 +0900

    learn about dataBinding

--- app/src/main/java/com/androidlearning/dice_lecture/MainActivity.kt
+++ app/src/main/java/com/androidlearning/dice_lecture/MainActivity.kt
@@ -2,10 +2,30 @@ package com.androidlearning.dice_lecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.androidlearning.dice_lecture.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //1번방식 - findViewById
+//        val btn = findViewById<Button>(R.id.testBtnId)
+//        btn.setOnClickListener{
+//            Toast.makeText(this, "click", Toast.LENGTH_LONG).show()
+//        }
+
+        //2번방식 - DataBinding
+        binding.testBtnId.setOnClickListener{
+            Toast.makeText(this, "click", Toast.LENGTH_LONG).show()
+        }
     }
 }
